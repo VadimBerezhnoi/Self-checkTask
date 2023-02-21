02ea354a30ab10390f637f4f475dc500cb160f2c
VadimBerezhnoi <tsigma66@gmail.com>
2023-02-21 23:11:38 +0300

    Решена задача с выводом суммы цифр в числе

---
+++ Task_002/Task_002.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+int SumDigits(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в {num} равна {SumDigits(num)}");
